beef9b27b599eadf17a5340fd88471c1b05d84b5
Lorenna-MB <lorenna.batista@run2biz.com>
2025-07-29 11:27:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-/*const input = document.querySelector('.login-input');
-const buttonPokemon = document.querySelector('.loginPokemon');
-const buttonHxh = document.querySelector('.loginHxh');
-const form= document.querySelector('.login-form');
-
-const validateInput = ({ target }) => {
-    .log(event.target.value); Aparece o valor digitado no f12
-    if (target.value.length > 1){
-        buttonHxh.removeAttribute('disabled');
-        buttonPokemon.removeAttribute('disabled');
-        return;
-    }
-
-    buttonHxh.setAttribute('disabled', '');
-    buttonPokemon.setAttribute('disabled', '');
-}
-
-const handleSubmit = (event) =>{
-    event.preventDefault(); /*bloqueia o comportamento padrão da página de recarregar
-
-    localStorage.setItem('player', input.value);
-    window.location = 'pages/index.html'
-}
-
-input.addEventListener('input', validateInput);
-form.addEventListener('submit', handleSubmit); */
-
 const input = document.querySelector('.login-input');
 const buttonPokemon = document.querySelector('.loginPokemon');
 const buttonHxh = document.querySelector('.loginHxh');
