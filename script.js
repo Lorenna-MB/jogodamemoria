91f2b5c71eb724c7ac56d60b159af966e4ed6fb2
Lorenna <lorenna.batista@run2biz.com>
2025-07-31 10:43:20 -0300

    Criação de show modal para seleção de tema do jogo da memória

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const input = document.querySelector('.login-input');
 const buttonPokemon = document.querySelector('.loginPokemon');
 const buttonHxh = document.querySelector('.loginHxh');
 const form = document.querySelector('.login-form');
+const openModalButton = document.querySelector('.open-modal');
 
 const validateInput = ({ target }) => {
     const isValid = target.value.length > 1 && target.value.length <= 15;
 
     buttonHxh.disabled = !isValid;
     buttonPokemon.disabled = !isValid;
+    openModalButton.disabled = !isValid;
 };
 
 const handleClick = (destination) => {
@@ -23,4 +25,25 @@ buttonPokemon.addEventListener('click', () => {
 
 buttonHxh.addEventListener('click', () => {
     handleClick('./memoryHunter/hunter.html');
+});
+
+const openButtons = document.querySelectorAll('.open-modal');
+openButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+        modal.classList.add('active');
+        modal.showModal();
+    });
+});
+
+const closeButtons = document.querySelectorAll('.close-modal');
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+        modal.classList.remove('active');
+        modal.close();
+    });
 });
